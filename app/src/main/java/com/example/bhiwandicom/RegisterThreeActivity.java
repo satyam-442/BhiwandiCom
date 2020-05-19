46b453b1a2713bddcc9c6041e0d73605699a0a1f
satyam-442 <satyamtiwari210@gmail.com>
2020-05-19 13:23:42 -0700

    Updation of project

--- app/src/main/java/com/example/bhiwandicom/RegisterThreeActivity.java
+++ app/src/main/java/com/example/bhiwandicom/RegisterThreeActivity.java
@@ -5,13 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterThreeActivity extends AppCompatActivity {
+
+    Button signup_next_button;
+    TextInputLayout acceptNoFromUser;
+    String gender, age, fullname, email, phone, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_three);
+
+        fullname = getIntent().getStringExtra("fullname");
+        email = getIntent().getStringExtra("email");
+        phone = getIntent().getStringExtra("phone");
+        password = getIntent().getStringExtra("password");
+        gender = getIntent().getStringExtra("gender");
+        age = getIntent().getStringExtra("age");
+
+        acceptNoFromUser = findViewById(R.id.acceptNoFromUser);
+        acceptNoFromUser.getEditText().setText(phone);
+
+        signup_next_button = findViewById(R.id.signup_next_button);
+        signup_next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterThreeActivity.this, VerifyPhoneActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void backToWelcome(View view) {
@@ -21,8 +47,6 @@ public class RegisterThreeActivity extends AppCompatActivity {
     }
 
     public void CallNextRegisterScreen(View view) {
-        Intent intent = new Intent(this, VerifyPhoneActivity.class);
-        startActivity(intent);
-        finish();
+
     }
 }
--- app/src/main/java/com/example/bhiwandicom/RegisterTwoActivity.java
+++ app/src/main/java/com/example/bhiwandicom/RegisterTwoActivity.java
@@ -3,14 +3,20 @@ package com.example.bhiwandicom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -22,6 +28,10 @@ public class RegisterTwoActivity extends AppCompatActivity {
     Button next, login;
     TextView titleText;
     String gender, age, fullname, email, phone, password;
+    EditText genderText, ageText;
+    DatePicker picker;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +44,47 @@ public class RegisterTwoActivity extends AppCompatActivity {
         login = findViewById(R.id.signup_login_button);
         titleText = findViewById(R.id.signup_title_text);
 
+        radioGroup = findViewById(R.id.genderGroup);
         fullname = getIntent().getStringExtra("fullname");
         email = getIntent().getStringExtra("email");
         phone = getIntent().getStringExtra("phone");
         password = getIntent().getStringExtra("password");
 
+        genderText = findViewById(R.id.genderText);
+        ageText = findViewById(R.id.ageText);
+
+        picker = findViewById(R.id.ageDatePicker);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ageText.setText(picker.getDayOfMonth()+"/"+(picker.getMonth() + 1) + "/" + picker.getYear());
+                Intent intent = new Intent(RegisterTwoActivity.this, RegisterThreeActivity.class);
+                intent.putExtra("fullname",fullname);
+                intent.putExtra("email",email);
+                intent.putExtra("phone",phone);
+                intent.putExtra("password",password);
+                intent.putExtra("gender",genderText.getText().toString());
+                intent.putExtra("age",ageText.getText().toString());
+                Pair[] pairs = new Pair[4];
+                pairs[0] = new Pair<View,String>(backButton,"transition_back_btn");
+                pairs[1] = new Pair<View,String>(titleText,"transition_title_text");
+                pairs[2] = new Pair<View,String>(next,"transition_next_btn");
+                pairs[3] = new Pair<View,String>(login,"transition_login_btn");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RegisterTwoActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+            }
+        });
+    }
+
+    public void checkButtom(View view)
+    {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+
+        //toast = new Toast(this);
+        /*Toast.makeText(this, radioButton.getText(), Toast.LENGTH_SHORT).show();*/
+        genderText.setText(radioButton.getText());
     }
 
     public void backToWelcome(View view) {
